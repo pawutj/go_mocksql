14b7a65526183094c4d58288a6d5152224841563
astralaria <pawut.j@gmail.com>
2023-01-04 02:46:36 +0700

    Add Create FindAll CASe

--- sql.go
+++ sql.go
@@ -6,6 +6,12 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+type User struct {
+	ID   int
+	Name string
+	Age  int
+}
+
 func recordStats(db *sql.DB, userID, productID int64) (err error) {
 	tx, err := db.Begin()
 	if err != nil {
@@ -28,6 +34,40 @@ func recordStats(db *sql.DB, userID, productID int64) (err error) {
 		return
 	}
 	return
+}
+
+func CreateUsers(db *sql.DB, user User) (User, error) {
+
+	row := db.QueryRow(`INSERT INTO users (name, age) values ($1, $2)  RETURNING id,name,age`, user.Name, user.Age)
+
+	result := User{}
+	err := row.Scan(&result.ID, &result.Name, &result.Age)
+	if err != nil {
+		return User{}, err
+	}
+
+	return result, nil
+}
+
+func FindAllUsers(db *sql.DB) ([]User, error) {
+	rows, err := db.Query("SELECT id, name, age FROM USERS")
+
+	result := []User{}
+
+	if err != nil {
+		return result, err
+	}
+
+	for rows.Next() {
+		user := User{}
+		err := rows.Scan(&user.ID, &user.Name, &user.Age)
+		if err != nil {
+			return result, err
+		}
+		result = append(result, user)
+	}
+	return result, nil
+
 }
 
 func main() {
--- sql_test.go
+++ sql_test.go
@@ -1,58 +1,86 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/assert"
 )
 
-// a successful case
-func TestShouldUpdateStats(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
-	defer db.Close()
+// // a successful case
+// func TestShouldUpdateStats(t *testing.T) {
+// 	db, mock, err := sqlmock.New()
+// 	if err != nil {
+// 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+// 	}
+// 	defer db.Close()
 
-	mock.ExpectBegin()
-	mock.ExpectExec("UPDATE products").WillReturnResult(sqlmock.NewResult(1, 1))
-	mock.ExpectExec("INSERT INTO product_viewers").WithArgs(2, 3).WillReturnResult(sqlmock.NewResult(1, 1))
-	mock.ExpectCommit()
+// 	mock.ExpectBegin()
+// 	mock.ExpectExec("UPDATE products").WillReturnResult(sqlmock.NewResult(1, 1))
+// 	mock.ExpectExec("INSERT INTO product_viewers").WithArgs(2, 3).WillReturnResult(sqlmock.NewResult(1, 1))
+// 	mock.ExpectCommit()
 
-	// now we execute our method
-	if err = recordStats(db, 2, 3); err != nil {
-		t.Errorf("error was not expected while updating stats: %s", err)
-	}
+// 	// now we execute our method
+// 	if err = recordStats(db, 2, 3); err != nil {
+// 		t.Errorf("error was not expected while updating stats: %s", err)
+// 	}
 
-	// we make sure that all expectations were met
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Errorf("there were unfulfilled expectations: %s", err)
-	}
+// 	// we make sure that all expectations were met
+// 	if err := mock.ExpectationsWereMet(); err != nil {
+// 		t.Errorf("there were unfulfilled expectations: %s", err)
+// 	}
+// }
+
+// // a failing test case
+// func TestShouldRollbackStatUpdatesOnFailure(t *testing.T) {
+// 	db, mock, err := sqlmock.New()
+// 	if err != nil {
+// 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+// 	}
+// 	defer db.Close()
+
+// 	mock.ExpectBegin()
+// 	mock.ExpectExec("UPDATE products").WillReturnResult(sqlmock.NewResult(1, 1))
+// 	mock.ExpectExec("INSERT INTO product_viewers").
+// 		WithArgs(2, 3).
+// 		WillReturnError(fmt.Errorf("some error"))
+// 	mock.ExpectRollback()
+
+// 	// now we execute our method
+// 	if err = recordStats(db, 2, 3); err == nil {
+// 		t.Errorf("was expecting an error, but there was none")
+// 	}
+
+// 	// we make sure that all expectations were met
+// 	if err := mock.ExpectationsWereMet(); err != nil {
+// 		t.Errorf("there were unfulfilled expectations: %s", err)
+// 	}
+// }
+
+func TestCreateUser(t *testing.T) {
+	user := User{Name: "AnuchitO", Age: 19}
+	db, mock, _ := sqlmock.New()
+
+	rows := sqlmock.NewRows([]string{"id", "name", "age"}).AddRow(0, "AnuchitO", 19)
+
+	// mock.ExpectBegin()
+	mock.ExpectQuery("INSERT INTO users").WithArgs("AnuchitO", 19).WillReturnRows(rows)
+
+	result, err := CreateUsers(db, user)
+	assert.Nil(t, err)
+	assert.EqualValues(t, result.Name, user.Name)
+	assert.EqualValues(t, result.Age, user.Age)
+
 }
 
-// a failing test case
-func TestShouldRollbackStatUpdatesOnFailure(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
-	defer db.Close()
+func TestFindAllUser(t *testing.T) {
+	db, mock, _ := sqlmock.New()
+	rows := sqlmock.NewRows([]string{"id", "name", "age"}).AddRow(0, "SomeUser0", 0).AddRow(0, "SomeUser1", 1).AddRow(0, "SomeUser2", 2)
 
-	mock.ExpectBegin()
-	mock.ExpectExec("UPDATE products").WillReturnResult(sqlmock.NewResult(1, 1))
-	mock.ExpectExec("INSERT INTO product_viewers").
-		WithArgs(2, 3).
-		WillReturnError(fmt.Errorf("some error"))
-	mock.ExpectRollback()
+	// mock.ExpectBegin()
+	mock.ExpectQuery("SELECT id, name, age FROM USERS").WillReturnRows(rows)
 
-	// now we execute our method
-	if err = recordStats(db, 2, 3); err == nil {
-		t.Errorf("was expecting an error, but there was none")
-	}
-
-	// we make sure that all expectations were met
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Errorf("there were unfulfilled expectations: %s", err)
-	}
+	result, err := FindAllUsers(db)
+	assert.Nil(t, err)
+	assert.EqualValues(t, len(result), 3)
 }
